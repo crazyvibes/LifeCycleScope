c5a71861d20662d9820c61eb6c3d5d8d3631e5fd
crazyvibes <bkm123r@gmail.com>
2021-01-30 13:27:10 +0530

    Initial commit

---
+++ app/src/main/java/in/bk/lifecyclescope/MainActivity.kt
@@ -0,0 +1,38 @@
+package `in`.bk.lifecyclescope
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import `in`.bk.lifecyclescope.ui.main.MainFragment
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+
+//            lifecycleScope.launch {
+//                delay(5000)
+//                progressBar.visibility = View.VISIBLE
+//                delay(10000)
+//                progressBar.visibility = View.GONE
+//            }
+
+            lifecycleScope.launch(IO) {
+                Log.i("TAG", "Thread is :${Thread.currentThread().name} ")
+            }
+
+
+        }
+    }
+}
---
+++ app/src/main/java/in/bk/lifecyclescope/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package `in`.bk.lifecyclescope.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
